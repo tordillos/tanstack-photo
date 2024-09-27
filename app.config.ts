26240c09053254a78095c1937b7dd6c0c345f5ae
Pedro Blázquez <pedro.blazquez@wembleystudios.com>
2024-09-27 23:39:12 +0200

    chore: add tailwind

--- app.config.ts
+++ app.config.ts
@@ -1,8 +1,16 @@
 // app.config.ts
 import { defineConfig } from "@tanstack/start/config";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   server: {
     preset: "cloudflare-pages",
+  },
+  vite: {
+    plugins: () => [
+      tsConfigPaths({
+        projects: ["./tsconfig.json"],
+      }),
+    ],
   },
 });
--- app/routes/__root.tsx
+++ app/routes/__root.tsx
@@ -1,31 +1,33 @@
 // app/routes/__root.tsx
-import { createRootRoute } from '@tanstack/react-router'
-import { Outlet, ScrollRestoration } from '@tanstack/react-router'
-import { Body, Head, Html, Meta, Scripts } from '@tanstack/start'
-import * as React from 'react'
+import { createRootRoute } from "@tanstack/react-router";
+import { Outlet, ScrollRestoration } from "@tanstack/react-router";
+import { Body, Head, Html, Meta, Scripts } from "@tanstack/start";
+import * as React from "react";
+import appCss from "~/styles/app.css?url";
 
 export const Route = createRootRoute({
   meta: () => [
     {
-      charSet: 'utf-8',
+      charSet: "utf-8",
     },
     {
-      name: 'viewport',
-      content: 'width=device-width, initial-scale=1',
+      name: "viewport",
+      content: "width=device-width, initial-scale=1",
     },
     {
-      title: 'TanStack Start Starter',
+      title: "TanStack Start Starter",
     },
   ],
+  links: () => [{ rel: "stylesheet", href: appCss }],
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
     <RootDocument>
       <Outlet />
     </RootDocument>
-  )
+  );
 }
 
 function RootDocument({ children }: { children: React.ReactNode }) {
@@ -40,5 +42,5 @@ function RootDocument({ children }: { children: React.ReactNode }) {
         <Scripts />
       </Body>
     </Html>
-  )
+  );
 }
